cdfcc3ebec20fafd943737850529037fff36f87c
Gallyamov Vitaly <vitgvr@gmail.com>
2014-05-29 20:21:26 +0600

    new API functions

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -48,6 +48,80 @@ class ApiController extends FrontController
 		Yii::app()->end();
 	}
 
+	public function actionSavePart($id){
+		header('Content-Type: application/json');
+
+		$part = Parts::model()->findByPk($id);
+
+		if($part && isset($_POST['Part'])){
+			$part->attributes = $_POST['Part'];
+
+			//create name
+			$name = "";
+
+			if($part->category)
+				$name .= $part->category->name;
+			if($part->car_model && $part->car_model->car_brand)
+				$name .= ", ".$part->car_model->car_brand->name." ".$part->car_model->name;
+
+			$part->name = $name;
+
+			if($part->validate()){
+				$this->attachUsedCar($part);
+				$part->status = Parts::STATUS_PUBLISH;
+				$part->save(false);
+				$response->data['save'] = 1;
+			}else
+				$this->response->errors = $part->getErrors();
+		}
+
+		$this->printJSON();
+		
+		Yii::app()->end();
+	}
+
+	public function actionAddImage($id){
+
+		$part = Parts::model()->findByPk($id);
+		if($part){
+			$model = new GalleryPhoto();
+	        $model->gallery_id = $part->gallery_id;
+	        $imageFile = CUploadedFile::getInstanceByName('Image');
+	        if($imageFile){
+	        	var_dump($imageFile->getName());
+
+	        	if(!$part->gallery->main) $model->main = 1;
+
+	        	$model->file_name = $imageFile->getName();
+	        	$model->ext = $imageFile->extensionName;
+        		$model->save();
+
+        		$model->setImage($imageFile->getTempName());
+	        }
+		}
+
+		Yii::app()->end();
+	}
+
+	//attach used car to part
+	private function attachUsedCar($model){
+
+		if(isset($_POST['UsedCar'])){
+			$db = Yii::app()->db->createCommand();
+
+			if(!empty($_POST['UsedCar'])){
+				
+				if($model->usedCar) //already add then update
+					$db->update('{{Parts_UsedCars}}', array('used_car_id' => $_POST['UsedCar']), 'parts_id=:p', array(':p' => $model->id));
+				else //not add then insert
+					$db->insert('{{Parts_UsedCars}}', array('parts_id' => $model->id, 'used_car_id' => $_POST['UsedCar']));
+			}else{ 
+				if($model->usedCar) //delete relation
+					$db->delete('{{Parts_UsedCars}}', 'parts_id=:p', array(':p' => $model->id));
+			}
+		}
+	}
+
 	public function actionAuth(){
 		header('Content-Type: application/json');
 
@@ -135,6 +209,25 @@ class ApiController extends FrontController
 			->where('status=1')
 			->queryAll();
 		$this->response->data['bu_cars'] = $data;
+
+		$this->printJSON();
+
+		Yii::app()->end();
+	}
+
+	//date format %e.%m.%y
+	public function actionGetAllParts(){
+		header('Content-Type: application/json');
+
+		$data = Yii::app()->db->createCommand()
+			->select('p.id, name, DATE_FORMAT(create_time, "%e.%m.%y") as date, price_sell, price_buy, comment, category_id, car_model_id, location_id, supplier_id, u.used_car_id')
+			->from('{{Parts}} as p')
+			->leftJoin('{{Parts_UsedCars}} as u', 'p.id=u.parts_id')
+			->where('status=7') //DEVICE
+			->order('create_time DESC')
+			->queryAll();
+
+		$this->response->data['parts'] = $data;
 
 		$this->printJSON();
 
