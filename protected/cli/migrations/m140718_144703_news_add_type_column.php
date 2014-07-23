80adad9c87743c1a29f86c8b60d2d2dc280173e2
But A. S <scorgi86@gmail.com>
2014-07-23 13:16:41 +0400

    migrations table resister changed

--- protected/cli/migrations/m140718_144703_news_add_type_column.php
+++ protected/cli/migrations/m140718_144703_news_add_type_column.php
@@ -10,11 +10,11 @@ class m140718_144703_news_add_type_column extends CDbMigration
     // таблицы к удалению, можно использовать '{{table}}'
 	public function up()
     {
-        $this->addColumn('{{news}}', 'type', 'int COMMENT "Тип новости"');
+        $this->addColumn('{{News}}', 'type', 'int COMMENT "Тип новости"');
     }
 
     public function down()
     {
-        $this->dropColumn('{{news}}', 'type');
+        $this->dropColumn('{{News}}', 'type');
     }
 }
