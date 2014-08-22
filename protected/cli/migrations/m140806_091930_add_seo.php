bffd11e627dee941b4b54077edd1d7d4803f285f
DiggerZ <DiggerZ@mail.ru>
2014-08-22 14:23:05 +0600

    migracii

--- protected/cli/migrations/m140806_091930_add_seo.php
+++ protected/cli/migrations/m140806_091930_add_seo.php
@@ -10,23 +10,23 @@ class m140806_091930_add_seo extends CDbMigration
     // таблицы к удалению, можно использовать '{{table}}'
 	public function up()
     {
-        $this->addColumn('{{news}}', 'seo_id', 'int');
-        $this->addColumn('{{page}}', 'seo_id', 'int');
-        $this->addColumn('{{parts}}', 'seo_id', 'int');
-        $this->addColumn('{{usedCars}}', 'seo_id', 'int');
-        $this->addColumn('{{carBrands}}', 'seo_id', 'int');
-        $this->addColumn('{{categories}}', 'seo_id', 'int');
-        $this->addColumn('{{carModels}}', 'seo_id', 'int');
+        $this->addColumn('{{News}}', 'seo_id', 'int');
+        $this->addColumn('{{News}}', 'seo_id', 'int');
+        $this->addColumn('{{Parts}}', 'seo_id', 'int');
+        $this->addColumn('{{UsedCars}}', 'seo_id', 'int');
+        $this->addColumn('{{CarBrands}}', 'seo_id', 'int');
+        $this->addColumn('{{Categories}}', 'seo_id', 'int');
+        $this->addColumn('{{CarModels}}', 'seo_id', 'int');
     }
 
     public function down()
     {
-        $this->dropColumn('{{news}}', 'seo_id', 'int');
-        $this->dropColumn('{{page}}', 'seo_id', 'int');
-        $this->dropColumn('{{parts}}', 'seo_id', 'int');
-        $this->dropColumn('{{usedCars}}', 'seo_id', 'int');
-        $this->dropColumn('{{carBrands}}', 'seo_id', 'int');
-        $this->dropColumn('{{categories}}', 'seo_id', 'int');
-        $this->dropColumn('{{carModels}}', 'seo_id', 'int');
+        $this->dropColumn('{{News}}', 'seo_id', 'int');
+        $this->dropColumn('{{News}}', 'seo_id', 'int');
+        $this->dropColumn('{{Parts}}', 'seo_id', 'int');
+        $this->dropColumn('{{UsedCars}}', 'seo_id', 'int');
+        $this->dropColumn('{{CarBrands}}', 'seo_id', 'int');
+        $this->dropColumn('{{Categories}}', 'seo_id', 'int');
+        $this->dropColumn('{{CarModels}}', 'seo_id', 'int');
     }
 }
