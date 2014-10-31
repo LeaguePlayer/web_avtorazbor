7b3c149c5e634b5678a17c220824fd614f192717
DiggerZ <DiggerZ@mail.ru>
2014-10-31 10:55:09 +0600

    qwe

--- themes/default/views/search/_PartsItem.php
+++ themes/default/views/search/_PartsItem.php
@@ -11,11 +11,13 @@
     <a href="/detail/<?=$data->alias?>/<?=$data->id?>" class="link">
         <?=$data->car_model->name.' '.$data->category->name?>
     </a>
-    <span class="dsc">
-    	<strong>Коментарий:<br></strong>
-        <?=$data->comment?>
-    </span>
-    <span class="price">
-        <?= $data->price_sell ? number_format((int)$data->price_sell,0,' ',' ').' руб.' : ''?> 
-    </span>
+    <div>
+        <span class="desc">
+        	<strong>Коментарий:<br></strong>
+            <?=$data->comment?>
+        </span>
+        <span class="price">
+            <?= $data->price_sell ? number_format((int)$data->price_sell,0,' ',' ').' руб.' : ''?> 
+        </span>
+    </div>
 </div>
