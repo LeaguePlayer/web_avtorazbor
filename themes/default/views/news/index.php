a44c20cd189433e9fc52324bb8833b44f4ae57d2
DiggerZ <DiggerZ@mail.ru>
2014-11-12 14:27:33 +0600

    qwe

--- themes/default/views/news/index.php
+++ themes/default/views/news/index.php
@@ -1,38 +1,40 @@
 <div class="page">
-        	<h1 class="head">
-        		Новые автомобили в разборе
-        	</h1>
+	<h1 class="head">
+		Новые автомобили в разборе
+	</h1>
 
-        	<div class="wr">
+	<div class="wr">
 
-        			<div class="tabs">
-        				<ul>
-                            <li class="active">
-                                <a href="#tabs-3" >
-                                    Поступили в разбор
-                                </a>    
-                            </li>
-                            <li>
-                                <a href="#tabs-4" >
-                                    Поступили в продажу
-                                </a>    
-                            </li>
-        				</ul>
-        			</div>
+			<div class="tabs">
+				<ul>
+                    <li class="active">
+                        <a href="#tabs-3" >
+                            Поступили в продажу
+                            
+                        </a>    
+                    </li>
+                    <li>
+                        <a href="#tabs-4" >
+                            Поступили в разбор
+                        </a>    
+                    </li>
+				</ul>
+			</div>
 
-        			<div class="content clear">
+			<div class="content clear">
 
-                        <div class="news tab-active" id="tabs-3">
-                            <?
-                                $this->renderPartial('tabView',array('dataProvider'=>$razbor),false,true);
-                            ?>
-                        </div>
-                        <div class="news" id="tabs-4">
-                            <?
-                                $this->renderPartial('tabView',array('dataProvider'=>$cars),false,true);
-                            ?>
-                        </div>
-                        
-        			</div>
-        	</div>
-        </div>	
+                <div class="news  tab-active" id="tabs-3">
+                    <?
+                        $this->renderPartial('tabView',array('dataProvider'=>$cars),false,true);
+                    ?>
+                </div>
+                <div class="news" id="tabs-4">
+                    <?
+                        $this->renderPartial('tabView',array('dataProvider'=>$razbor),false,true);
+                    ?>
+                </div>
+                
+                
+			</div>
+	</div>
+</div>	
