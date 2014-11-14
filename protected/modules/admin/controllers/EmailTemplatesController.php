e9db7837aad45e48f6d77d676877a9399d3d4244
DiggerZ <DiggerZ@mail.ru>
2014-11-14 12:02:14 +0600

    qweg

--- protected/modules/admin/controllers/EmailTemplatesController.php
+++ protected/modules/admin/controllers/EmailTemplatesController.php
@@ -3,7 +3,18 @@
 class EmailTemplatesController extends AdminController
 {
 	public function actionGetAttributes($model){
-		$labels=$model::model()->attributeLabels();
+
+		$modelMap=array(
+			'questions'=>'Question',
+			'ownprice'=>'Ownprice',
+			'bookpart'=>'Bookpart',
+			'evackuator'=>'Evackuator',
+			'buyout'=>'Buyout',
+			'vacansycallback'=>'VacansyCallBack',
+		);
+
+		$labels=$modelMap[$model]::model()->attributeLabels();
+
 		echo "<p class=\"caption\">Вы можете использовать следующие марки</p><ul>";
 		foreach ($labels as $key => $label) {
 			echo "<li>{{$key}} - $label</li>";
