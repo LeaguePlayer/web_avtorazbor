74a8d55e9f9d87f3806ede39a91e361e9c828815
Sarvin <scorgi86@gmail.com>
2014-11-18 19:07:41 +0300

    qwe

--- protected/modules/admin/views/evackuator/update.php
+++ protected/modules/admin/views/evackuator/update.php
@@ -11,15 +11,4 @@ $this->menu=array(
 
 <h1><?php echo $model->translition(); ?> - Редактирование</h1>
 
-<?
-	$this->widget('zii.widgets.CDetailView', array(
-    'data'=>$model,
-    'attributes'=>array(
-        'fio',             // title attribute (in plain text)
-        'phone',        // an attribute of the related object "owner"
-        'email',        // an attribute of the related object "owner"
-        'vacansy.post',  // description attribute in HTML
-        'comment',  // description attribute in HTML
-    ),
-));
-?>
+<?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
