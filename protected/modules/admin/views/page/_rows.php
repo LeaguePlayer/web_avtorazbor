08ca073ee57a4bf871e23fea223742f13e5d55ce
DiggerZ <DiggerZ@mail.ru>
2014-11-26 13:06:41 +0600

    qwe

--- protected/modules/admin/views/page/_rows.php
+++ protected/modules/admin/views/page/_rows.php
@@ -21,4 +21,4 @@
 		<?php echo $form->error($model, 'wswg_body'); ?>
 	</div>
 
-	<?php if (!$_GET['system']) echo $form->dropDownListControlGroup($model, 'status', Page::getStatusAliases(), array('class'=>'span8', 'displaySize'=>1)); ?>
+	<?php if ($model->status==4) echo $form->dropDownListControlGroup($model, 'status', Page::getStatusAliases(), array('class'=>'span8', 'displaySize'=>1)); ?>
