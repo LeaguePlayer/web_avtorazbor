ed8d26fc3eda2239fef33e549173ac493a319b05
DiggerZ <DiggerZ@mail.ru>
2014-11-27 11:50:48 +0600

    qwe

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -247,5 +247,32 @@
     __cs.push(["setHost", "http://server.comagic.ru/comagic"]);
     </script>
     <script type="text/javascript" async src="http://app.comagic.ru/static/cs.min.js"></script>
+    <!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter27215654 = new Ya.Metrika({id:27215654,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/27215654" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 	</body>
 </html>
