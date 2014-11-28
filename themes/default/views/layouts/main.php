7a7e6fb6e9f902a9191fb44e39c366c1f87cf467
DiggerZ <DiggerZ@mail.ru>
2014-11-28 14:04:09 +0600

    qwe

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -245,11 +245,12 @@
     <?=$this->renderPartial('//account/login',array('model'=>new AuthForm),true)?>
     <?=$this->renderPartial('//forms/bookPart',array('model'=>new Bookpart));?>
     <script type="text/javascript">
-    var __cs = __cs || [];
-    __cs.push(["setAccount", "nvhuVPPprlE1nkOBxUt__QWZQAi8i_2G"]);
-    __cs.push(["setHost", "http://server.comagic.ru/comagic"]);
+        var __cs = __cs || [];
+        __cs.push(["setAccount", "Mi2IidVUExuAzLmZK2sfRxGtO8ooiD45"]);
+        __cs.push(["setHost", "http://server.comagic.ru/comagic"]);
     </script>
     <script type="text/javascript" async src="http://app.comagic.ru/static/cs.min.js"></script>
+
     <!-- Yandex.Metrika counter -->
 <script type="text/javascript">
 (function (d, w, c) {
